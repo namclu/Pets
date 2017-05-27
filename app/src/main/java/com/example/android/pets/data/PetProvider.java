745e4ff6027f8e032d07969e3436da2a583cc5d1
namclu <namlu1205@gmail.com>
2017-05-27 22:18:08 +1000

    4: Add ContentProvider getType() method in PetProvider
    - Add MIME types for list of pets and for a single pet
    in PetContract

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -74,7 +74,7 @@ public class PetProvider extends ContentProvider {
         Cursor cursor;
 
         // Check if the URI matcher matches the URI to a specific code
-        int match = sUriMatcher.match(uri);
+        final int match = sUriMatcher.match(uri);
 
         switch (match) {
             case PETS:
@@ -255,6 +255,16 @@ public class PetProvider extends ContentProvider {
      */
     @Override
     public String getType(Uri uri) {
-        return null;
+
+        final int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case PETS:
+                return PetEntry.CONTENT_LIST_TYPE;
+            case PET_ID:
+                return PetEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
+        }
     }
 }
